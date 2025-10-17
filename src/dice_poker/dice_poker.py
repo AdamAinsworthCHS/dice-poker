0c4497fe037a264b015547dfd382e3f79482c73b
AdamAinsworthCHS <147109448+AdamAinsworthCHS@users.noreply.github.com>
2025-10-17 09:46:51 -0700

    updated to snake case and added quit function

--- src/dice_poker/dice_poker.py
+++ src/dice_poker/dice_poker.py
@@ -11,11 +11,11 @@ class Card:
 		self.rank = rank
 		self.point_value = point_value
 		self.name = name
-	def getValue(self):
+	def get_value(self):
 		return self.rank
-	def getSuit(self):
+	def get_suit(self):
 		return self.suit
-	def pointValue(self):
+	def get_point_value(self):
 		return self.point_value
 	def __str__(self):
 		return self.name
@@ -77,6 +77,7 @@ cardsIndex =	{
 }
 
 
+#Main variables and lists
 hand = []
 playing = []
 score = 0
@@ -85,83 +86,105 @@ dice = 2
 
 #Draws a random card out of 52
 def draw_card():
-	cardID = random.randrange(0, 51)
-	drawCard = str(cardID)
-	cardList = list(cardsIndex[drawCard])
+	card_id = random.randrange(0, 51)
+	draw_card = str(card_id)
+	card_list = list(cardsIndex[draw_card])
 
-	newCard = Card(cardList[0], cardList[1], cardList[2], cardList[3])
-	return newCard
+	new_card = Card(card_list[0], card_list[1], card_list[2], card_list[3])
+	return new_card
 
 
 #Shows what cards are in hand and which are being played.
-def showCards():
+def show_cards():
 	print("")
 	print("Score: " + str(score))
-	print("Cards in Hand:")
 	print("Dice: " + str(dice))
+	print("Cards in Hand:")
 	for i in range (len(hand)):
 		print(str(i + 1) + ": " + str(hand[i]))
 	print("")
 	print("Cards to be Played:")
 	for i in range (len(playing)):
 		print(str(i + 1) + ": " + str(playing[i]))
-	gameProcess()
+	game_process()
+	return
 
 
 #Asks the player what action they want to take
-def gameProcess():
+def game_process():
 	global dice
-	handLength = len(hand)
-	cardsPlayLength = len(playing)
-	playCards = input("Draw Cards ('roll') Play Card ('#') Play Hand ('play'): ")
-	if playCards != "play" and playCards != "roll":
-		playCards = int(playCards)
-	else:
-		if playCards == "roll":
+	hand_length = len(hand)
+	cards_play_length = len(playing)
+	play_cards = input("Draw Cards ('roll') Play Card ('#') Play Hand ('play') Quit ('q'): ")
+	try:
+		play_cards = int(play_cards)
+	except ValueError:
+		if play_cards == "roll":
 			if dice >= 1:
 				dice -= 1
 				for i in range(random.randrange(1, 6)):
-					newCard = draw_card()
-					hand.append(newCard)
-				showCards()
+					new_card = draw_card()
+					hand.append(new_card)
+				show_cards()
 				return
 			else:
 				print("No dice remaining")
-				gameProcess()
-		else:
-			playHand()
+				game_process()
+				return
+		elif play_cards == "play":
+			play_hand()
 			return
-	if cardsPlayLength < 5:
-			if playCards > handLength or playCards <= 0:
+		elif play_cards == "q":
+			return
+		else:
+			print("Unrecognized command")
+			show_cards()
+			return
+	if cards_play_length < 5:
+			if play_cards > hand_length or play_cards <= 0:
 				print("Card not found")
-				gameProcess()
+				game_process()
+				return
 			else:
-				process(playCards)
+				process(play_cards)
+				return
 	else:
 		print("Max hand size is 5")
-		gameProcess()
-		
+		game_process()
+		return
+
+
 #Calculates the point value of played hand and updates the score
-def playHand():
+def play_hand():
 	global score
 	for i in range (len(playing)):
-		score = score + playing[i].pointValue()
+		score = score + playing[i].get_point_value()
 	playing.clear()
-	showCards()
+	show_cards()
+	return
+
 
 #Plays the card the player chose REWRITE NAME LATER
-def process(playCards):
-	playing.append(hand[int(playCards) - 1])
-	hand.remove(hand[int(playCards) - 1])
-	showCards()
+def process(play_cards):
+	playing.append(hand[int(play_cards) - 1])
+	hand.remove(hand[int(play_cards) - 1])
+	show_cards()
+	return
 
 
 def main():
+	global score
+	global dice
+	hand.clear()
+	playing.clear()
+	score = 0
+	dice = 2
 	for i in range(8):
-		newCard = draw_card()
-		hand.append(newCard)
-	showCards()
+		new_card = draw_card()
+		hand.append(new_card)
+	show_cards()
+	return
 
 
 if __name__ == "__main__":
-		main()
+	main()
