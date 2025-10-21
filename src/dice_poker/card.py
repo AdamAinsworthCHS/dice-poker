3959440a5262be43999538aa670f085422ad52fb
AdamAinsworthCHS <147109448+AdamAinsworthCHS@users.noreply.github.com>
2025-10-21 08:28:31 -0700

    separated class from main code

---
+++ src/dice_poker/card.py
@@ -0,0 +1,19 @@
+"""
+Card class
+By Adam Ainsworth
+"""
+
+class Card:
+	def __init__(self, suit, rank, point_value, name):
+		self.suit = suit
+		self.rank = rank
+		self.point_value = point_value
+		self.name = name
+	def get_value(self):
+		return self.rank
+	def get_suit(self):
+		return self.suit
+	def get_point_value(self):
+		return self.point_value
+	def __str__(self):
+		return self.name
--- src/dice_poker/dice_poker.py
+++ src/dice_poker/dice_poker.py
@@ -3,22 +3,9 @@ Dice Poker Game
 by Adam Ainsworth
 """
 import random
+from card import Card
 
 
-class Card:
-	def __init__(self, suit, rank, point_value, name):
-		self.suit = suit
-		self.rank = rank
-		self.point_value = point_value
-		self.name = name
-	def get_value(self):
-		return self.rank
-	def get_suit(self):
-		return self.suit
-	def get_point_value(self):
-		return self.point_value
-	def __str__(self):
-		return self.name
 
 
 cardsIndex =	{
