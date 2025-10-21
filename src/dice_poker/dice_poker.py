8e728554a731fa684151fdb89389732379bb5cad
AdamAinsworthCHS <147109448+AdamAinsworthCHS@users.noreply.github.com>
2025-10-21 08:38:50 -0700

    streamlined card drawing system

--- src/dice_poker/dice_poker.py
+++ src/dice_poker/dice_poker.py
@@ -8,59 +8,27 @@ from card import Card
 
 
 
-cardsIndex =	{
-	"0": ("spade", "2", 2, "2 of Spades"),
-	"1": ("spade", "3", 3, "3 of Spades"),
-	"2": ("spade", "4", 4, "4 of Spades"),
-	"3": ("spade", "5", 5, "5 of Spades"),
-	"4": ("spade", "6", 6, "6 of Spades"),
-	"5": ("spade", "7", 7, "7 of Spades"),
-	"6": ("spade", "8", 8, "8 of Spades"),
-	"7": ("spade", "9", 9, "9 of Spades"),
-	"8": ("spade", "10", 10, "10 of Spades"),
-	"9": ("spade", "jack", 11, "Jack of Spades"),
-	"10": ("spade", "queen", 12, "Queen of Spades"),
-	"11": ("spade", "king", 13, "King of Spades"),
-	"12": ("spade", "ace", 14, "Ace of Spades"),
-	"13": ("club", "2", 2, "2 of Clubs"),
-	"14": ("club", "3", 3, "3 of Clubs"),
-	"15": ("club", "4", 4, "4 of Clubs"),
-	"16": ("club", "5", 5, "5 of Clubs"),
-	"17": ("club", "6", 6, "6 of Clubs"),
-	"18": ("club", "7", 7, "7 of Clubs"),
-	"19": ("club", "8", 8, "8 of Clubs"),
-	"20": ("club", "9", 9, "9 of Clubs"),
-	"21": ("club", "10", 10, "10 of Clubs"),
-	"22": ("club", "jack", 11, "Jack of Clubs"),
-	"23": ("club", "queen", 12, "Queen of Clubs"),
-	"24": ("club", "king", 13, "King of Clubs"),
-	"25": ("club", "ace", 14, "Ace of Clubs"),
-	"26": ("diamond", "2", 2, "2 of Diamonds"),
-	"27": ("diamond", "3", 3, "3 of Diamonds"),
-	"28": ("diamond", "4", 4, "4 of Diamonds"),
-	"29": ("diamond", "5", 5, "5 of Diamonds"),
-	"30": ("diamond", "6", 6, "6 of Diamonds"),
-	"31": ("diamond", "7", 7, "7 of Diamonds"),
-	"32": ("diamond", "8", 8, "8 of Diamonds"),
-	"33": ("diamond", "9", 9, "9 of Diamonds"),
-	"34": ("diamond", "10", 10, "10 of Diamonds"),
-	"35": ("diamond", "jack", 11, "Jack of Diamonds"),
-	"36": ("diamond", "queen", 12, "Queen of Diamonds"),
-	"37": ("diamond", "king", 13, "King of Diamonds"),
-	"38": ("diamond", "ace", 14, "Ace of Diamonds"),
-	"39": ("heart", "2", 2, "2 of Hearts"),
-	"40": ("heart", "3", 3, "3 of Hearts"),
-	"41": ("heart", "4", 4, "4 of Hearts"),
-	"42": ("heart", "5", 5, "5 of Hearts"),
-	"43": ("heart", "6", 6, "6 of Hearts"),
-	"44": ("heart", "7", 7, "7 of Hearts"),
-	"45": ("heart", "8", 8, "8 of Hearts"),
-	"46": ("heart", "9", 9, "9 of Hearts"),
-	"47": ("heart", "10", 10, "10 of Hearts"),
-	"48": ("heart", "jack", 11, "Jack of Hearts"),
-	"49": ("heart", "queen", 12, "Queen of Hearts"),
-	"50": ("heart", "king", 13, "King of Hearts"),
-	"51": ("heart", "ace", 14, "Ace of Hearts"),
+cards_index =	{
+	"2": ("2"),
+	"3": ("3"),
+	"4": ("4"),
+	"5": ("5"),
+	"6": ("6"),
+	"7": ("7"),
+	"8": ("8"),
+	"9": ("9"),
+	"10": ("10"),
+	"11": ("Jack"),
+	"12": ("Queen"),
+	"13": ("King"),
+	"14": ("Ace")
+}
+
+suit_index =	{
+	"1": ("Spades"),
+	"2": ("Diamonds"),
+	"3": ("Clubs"),
+	"4": ("Hearts"),
 }
 
 
@@ -73,11 +41,13 @@ dice = 2
 
 #Draws a random card out of 52
 def draw_card():
-	card_id = random.randrange(0, 51)
-	draw_card = str(card_id)
-	card_list = list(cardsIndex[draw_card])
+	card_suit_id = random.randrange(1, 4)
+	card_value = random.randrange(2, 14)
+	card_suit = suit_index[str(card_suit_id)]
+	card_rank = cards_index[str(card_value)]
+	card_name = card_rank + " of " + card_suit
 
-	new_card = Card(card_list[0], card_list[1], card_list[2], card_list[3])
+	new_card = Card(card_suit, card_rank, card_value, card_name)
 	return new_card
 
 
