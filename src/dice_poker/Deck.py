411e5e72ba2ee34ad5c22e51f94030c593a421ff
AdamAinsworthCHS <147109448+AdamAinsworthCHS@users.noreply.github.com>
2025-11-04 09:25:51 -0800

    fixed redundant tuples in dictionary

--- src/dice_poker/Deck.py
+++ src/dice_poker/Deck.py
@@ -7,25 +7,25 @@ from Card import Card
 
 class Deck:
 	cards_index =   {
-			"2": ("2"),
-			"3": ("3"),
-			"4": ("4"),
-			"5": ("5"),
-			"6": ("6"),
-			"7": ("7"),
-			"8": ("8"),
-			"9": ("9"),
-			"10": ("10"),
-			"11": ("Jack"),
-			"12": ("Queen"),
-			"13": ("King"),
-			"14": ("Ace")
+			2: "2",
+			3: "3",
+			4: "4",
+			5: "5",
+			6: "6",
+			7: "7",
+			8: "8",
+			9: "9",
+			10: "10",
+			11: "Jack",
+			12: "Queen",
+			13: "King",
+			14: "Ace"
 		}
 	suit_index =    {
-			"1": ("Spades"),
-			"2": ("Diamonds"),
-			"3": ("Clubs"),
-			"4": ("Hearts")
+			1: "Spades",
+			2: "Diamonds",
+			3: "Clubs",
+			4: "Hearts"
 		}
 	def __init__(self, card_amount):
 		self.card_amount = card_amount
@@ -50,8 +50,8 @@ class Deck:
 	def draw_card():
 		card_suit_id = random.randrange(1, 5)
 		card_value = random.randrange(2, 15)
-		card_suit = Deck.suit_index[str(card_suit_id)]
-		card_rank = Deck.cards_index[str(card_value)]
+		card_suit = Deck.suit_index[card_suit_id]
+		card_rank = Deck.cards_index[card_value]
 		card_name = card_rank + " of " + card_suit
 
 		new_card = Card(card_suit, card_rank, card_value, card_name)
